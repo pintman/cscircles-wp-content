b138046b10b2ac844ef434a44d9ef75f2609914b
Marco Bakera <marco@bakera.de>
2018-03-09 19:51:10 +0100

    removed old files

--- docker/config-localhost.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_NAME', 'wordpress');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'MYSQL_ROOT_PASSWORD');
-define('DB_HOST', 'mysql');
-define('WP_CONTENT_DIR', '/usr/share/wordpress/wp-content');
-?>
--- docker/wordpress.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE wordpress;
-
--- TODO needed?
--- GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER
---  ON wordpress.*
---  TO wordpress@localhost
---  IDENTIFIED BY 'yourpasswordhere';
-
--- FLUSH PRIVILEGES;
